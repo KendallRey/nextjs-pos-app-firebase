4148a8ba8038efa46b1246631f5cd7dd98a33a53
KendallRey <107897863+KendallRey@users.noreply.github.com>
2024-07-16 12:55:43 +0800

    fix sample box component, added displayName

--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -7,9 +7,8 @@ import MuiBox, { BoxProps } from "@mui/material/Box";
 
 const BoxComponent = React.forwardRef((props: BoxProps, ref) => <MuiBox {...props} ref={ref} />);
 
-BoxComponent.displayName = 'BoxComponent'
+BoxComponent.displayName = "BoxComponent";
 
 const Box = motion(BoxComponent);
-
 
 export default Box;
